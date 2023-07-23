9d69f7ef88147d470f936b56f390ff9324a2e71f
yash jain <yash1620004541@gmail.com>
2023-07-24 00:20:08 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "e43dcb9605bb06714f4b9eca36c2d098",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
